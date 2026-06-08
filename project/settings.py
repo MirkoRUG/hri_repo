97093b9ba71ab54e8c25ce54e1012576f134f00a
Mirko <m.margaira@student.rug.nl>
2026-06-08 13:13:00 +0200

    sanitizing the DEBUG variable form the .env file

--- project/settings.py
+++ project/settings.py
@@ -1,17 +1,19 @@
 import logging
 import os
+
 from openai import OpenAI
+
 
 # not the best way to do a config, but it works
 def init():
     logging.basicConfig(level=logging.INFO)
     logging.getLogger("DeepFace").setLevel(logging.WARNING)
-    logging.getLogger("deepface").setLevel(logging.WARNING) 
-    logging.getLogger("httpx").setLevel(logging.WARNING) 
+    logging.getLogger("deepface").setLevel(logging.WARNING)
+    logging.getLogger("httpx").setLevel(logging.WARNING)
 
     global debug
     global client
     global model
-    debug = os.environ.get('DEBUG', 'False').lower() == 'true'
+    debug = os.environ.get("DEBUG", "False").strip().lower() == "true"
     client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
     model = os.environ.get("OPENAI_MODEL", "gpt-4o-mini")
